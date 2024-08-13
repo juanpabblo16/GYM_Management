0e9f103dd8ad7d7d6f7c5413884d7a12245a8a44
sebastian411 <jdiaz@vozy.co>
2024-08-12 21:07:59 -0500

    chore: Add Trainer microservice files and configurations

---
+++ Trainer_microservice/src/main/java/com/icesi/edu/co/TrainerMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.icesi.edu.co;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainerMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainerMicroserviceApplication.class, args);
+	}
+
+}
