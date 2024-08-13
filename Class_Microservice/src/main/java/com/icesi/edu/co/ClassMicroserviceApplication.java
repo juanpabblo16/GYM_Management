6dc535ce643292c3da62d09a50df2d36c421fb4c
JesusGarce22 <jesus02garces@gmail.com>
2024-08-12 21:20:15 -0500

    Class microservice

---
+++ Class_Microservice/src/main/java/com/icesi/edu/co/ClassMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.icesi.edu.co;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassMicroserviceApplication.class, args);
+	}
+
+}
---
+++ Class_Microservice/src/test/java/com/icesi/edu/co/ClassMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icesi.edu.co;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
