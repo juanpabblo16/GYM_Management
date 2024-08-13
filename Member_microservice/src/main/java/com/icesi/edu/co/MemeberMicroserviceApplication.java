e50097e197122031a8507b99dcfb5109e70f99f0
Juan Pablo Acevedo Mira <83412917+juanpabblo16@users.noreply.github.com>
2024-08-12 20:39:08 -0500

    Add files via upload

---
+++ Member_microservice/src/main/java/com/icesi/edu/co/MemeberMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.icesi.edu.co;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeberMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemeberMicroserviceApplication.class, args);
+	}
+
+}
---
+++ Member_microservice/src/test/java/com/icesi/edu/co/MemeberMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icesi.edu.co;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeberMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
